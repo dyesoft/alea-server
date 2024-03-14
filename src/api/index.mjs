0b1f18e08537b88ea98238322265b9c64b09f4b5
William Dye <will2dye4@mac.com>
2024-03-14 17:59:40 -0400

    Export default names from API modules

--- src/api/index.mjs
+++ src/api/index.mjs
@@ -3,15 +3,20 @@ export * as common from './common.mjs';
 
 export * from './game.mjs';
 export * as game from './game.mjs';
+export { default as GameAPI } from './game.mjs';
 
 export * from './player.mjs';
 export * as player from './player.mjs';
+export { default as PlayerAPI } from './player.mjs';
 
 export * from './room.mjs';
 export * as room from './room.mjs';
+export { default as RoomAPI } from './room.mjs';
 
 export * from './roomLinkRequest.mjs';
 export * as roomLinkRequest from './roomLinkRequest.mjs';
+export { default as RoomLinkRequestAPI } from './roomLinkRequest.mjs';
 
 export * from './status.mjs';
 export * as status from './status.mjs';
+export { default as StatusAPI } from './status.mjs';
